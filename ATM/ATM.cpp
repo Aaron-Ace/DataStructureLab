07df895aafd2e1d033ecf9a6a1cb310c2df8cc3b
Aaronace <andrewzhang0910@gmail.com>
2019-09-18 01:09:17 +0800

    Complete program waiting debug to pass the judge

--- ATM/ATM.cpp
+++ ATM/ATM.cpp
@@ -5,6 +5,30 @@ using namespace std;
 
 char passwd[12];
 int balance;
+int whetherLogin = 1;
+
+bool CheckPasswd(char pwd[])
+{
+    int len_passwd=0;
+    for(int j=0; passwd[j]; ++j){len_passwd++;}
+    int len_pwd = 0;
+    for(int j=0; pwd[j]; ++j){len_pwd++;}
+
+    //cout<<"1."<<len_passwd<<endl;
+    //cout<<"2."<<len_pwd<<endl;
+
+    if(len_passwd == len_pwd)
+    {
+        for(int i=0;i<len_passwd;i++)
+        {
+            int flag = 0;
+            if(passwd[i] != pwd[i]){flag = 1;}
+            if(flag == 0){return true;}
+            else{return false;}
+        }
+    }
+    else{return false;}
+}
 
 void Action(int x)
 {
@@ -13,7 +37,7 @@ void Action(int x)
         case 1:
             char pwd[12];
             cin>>pwd;
-            if(pwd){cout<<"Login successfully!"<<endl;}
+            if(CheckPasswd(pwd) == true){cout<<"Login successfully!"<<endl;whetherLogin = 0;}
             else{cout<<"Password is incorrect"<<endl;}
             break;
         case 2:
@@ -41,14 +65,15 @@ int main()
     int action;
     while(cin>>passwd>>balance)
     {
+        whetherLogin = 1;
         while(cin>>action)
         {
            if(action==0){break;}
-           else if(action)
+           else if(action == 1 || whetherLogin == 0)
            {
-
                Action(action);
            }
+           else{cout<<"Login First"<<endl;}
         }
     }
 }
