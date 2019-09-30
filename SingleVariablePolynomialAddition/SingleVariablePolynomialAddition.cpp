cb69e9471a65de9c6cec7cca4672e57c993124c8
Aaronace <andrewzhang0910@gmail.com>
2019-09-30 18:01:54 +0800

    Mostly complete, lack of sort function

--- SingleVariablePolynomialAddition/SingleVariablePolynomialAddition.cpp
+++ SingleVariablePolynomialAddition/SingleVariablePolynomialAddition.cpp
@@ -12,68 +12,137 @@
 #include<string>
 #include<algorithm>
 #include<sstream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-class Polynomial
+class Polynomial_A
 {
     public:
         float coefficient;
         int pow;
 };
 
-bool compare(Polynomial a, Polynomial b)
+class Polynomial_B
 {
-	return a.pow > b.pow;
+    public:
+        float coefficient;
+        int pow;
+};
+
+class Polynomial_C
+{
+    public:
+        float coefficient;
+        int pow;
+};
+
+
+float int2str(string num){
+    float res;
+    stringstream stream(num);
+    stream>>res;
+    return res;
+}
+
+int cmp(const void * a, const void * b)
+{
+
+    Polynomial_C *x = (Polynomial_C *)a;
+
+    Polynomial_C *y = (Polynomial_C *)b;
+
+    return (x->pow - y->pow);
+
 }
 
 int main()
 {
-    Polynomial polynomial[1000];
-    string input;
-    stringstream ss;
+    string input_A,input_B,temp;
     int array_coefficient[1000]={0};
-    while(getline(cin,input))
+    while(getline(cin,input_A))
     {
-        ss<<input;
-        int level=0;
-        while(ss)
+
+        Polynomial_A polynomial_a[1000];
+        Polynomial_B polynomial_b[1000];
+        Polynomial_C polynomial_c[1000];
+        stringstream ss_A,ss_B;
+
+        //cout<<"Cpoy:"<<input_A<<endl;
+        getline(cin,input_B);
+        //cout<<"Cpoy:"<<input_B<<endl;
+        ss_A<<input_A;
+        int level_A=0;
+        while(ss_A>>input_A)
         {
-            cin>>polynomial[level].coefficient;
-            cin>>polynomial[level].pow;
-            level += 1;
+            //cout<<input_A<<endl;
+            polynomial_a[level_A].coefficient = int2str(input_A) ;
+            ss_A>>input_A;
+            polynomial_a[level_A].pow = int2str(input_A);
+            level_A += 1;
         }
-        getline(cin,input);
-        ss<<input;
-        while(ss)
+        //cout<<"level_A:"<<level_A<<endl;
+        ss_B<<input_B;
+        int level_B=0;
+        while(ss_B>>input_B)
         {
-            cin>>polynomial[level].coefficient;
-            cin>>polynomial[level].pow;
-            int flag = 0;
-            for(int i=0 ;i<level;i++)
+            polynomial_b[level_B].coefficient = int2str(input_B);
+            ss_B>>input_B;
+            polynomial_b[level_B].pow = int2str(input_B);
+            level_B += 1;
+        }
+        //cout<<"level_B:"<<level_B<<endl;
+
+        int level_C =0;
+
+        for(int i=0;i<level_A;i++)
+        {
+            int flag =0;
+            for(int j=0;j<level_B;j++)
             {
-                if(polynomial[i].pow==polynomial[level].pow)
+                if(polynomial_a[i].pow==polynomial_b[j].pow)
                 {
-                    polynomial[i].coefficient += polynomial[level].coefficient;
-                    polynomial[level].coefficient = 0;
-                    polynomial[level].pow = 0;
-                    flag += 1;
+                    polynomial_c[level_C].coefficient = polynomial_a[i].coefficient + polynomial_b[j].coefficient;
+                    polynomial_c[level_C].pow = polynomial_a[i].pow;
+                    level_C += 1;
+                    flag =1;
                 }
             }
             if(flag==0)
             {
-                level+=1;
+                polynomial_c[level_C].coefficient = polynomial_a[i].coefficient;
+                polynomial_c[level_C].pow = polynomial_a[i].pow;
+                level_C += 1;
             }
+
         }
 
-        sort(polynomial[0].pow,polynomial[0+level].pow,compare);
-
-        for(int i=0;i<level;i++)
+        for(int i=0;i<level_B;i++)
         {
-           cout<< polynomial[0].pow ;
+            int flag =0;
+            for(int j=0;j<level_C;j++)
+            {
+                if(polynomial_b[i].pow==polynomial_c[j].pow)
+                {
+                    flag =1;
+                }
+            }
+            if(flag==0)
+            {
+                polynomial_c[level_C].coefficient = polynomial_b[i].coefficient;
+                polynomial_c[level_C].pow = polynomial_b[i].pow;
+                level_C += 1;
+            }
+
+        }
+
+        //sort(polynomial_c,sizeof(polynomial_c),cmp);
+
+        for(int i=0;i<level_C;i++)
+        {
+            cout<<polynomial_c[i].coefficient<<" "<<polynomial_c[i].pow<<" ";
         }
         cout<<endl;
-
-
     }
+    return 0;
 }
