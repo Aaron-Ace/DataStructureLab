09ddc722407e2864aa68dc76a8c6ee8160f30631
Aaronace <andrewzhang0910@gmail.com>
2019-10-31 02:16:01 +0800

    Change the input method from string to char ->Pass the judge

--- Coupling/Coupling.cpp
+++ Coupling/Coupling.cpp
@@ -18,19 +18,19 @@ int main()
     cin>>NumOfLoop;
     for(int j=0;j<NumOfLoop;j++)
     {
-        string string_input;
-        cin>>string_input;
+        char input[256]={0};
+        cin>>input;
+        //cout<<input;
         int length_input;
         int count_ans=0;
-        int count_B=0,count_G=0;
-        length_input = string_input.length();
-        for(int i=0;i<length_input;i++)
+        int count_B=0;
+        for(int i=0;i<256;i++)
         {
-            if(string_input[i]=='B')
+            if(input[i]=='B')
             {
                 count_B+=1;
             }
-            else if(string_input[i]=='G')
+            else if(input[i]=='G')
             {
                 if(count_B>0){count_ans+=1;count_B-=1;}
             }
