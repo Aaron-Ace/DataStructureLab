d373a0d92321e736b38952c8014eccec7e4e993a
Aaronace <andrewzhang0910@gmail.com>
2019-12-08 16:01:05 +0800

    Compiler error with correct ans

--- PostfixArithmeticOperation/PostfixArithmeticOperation.cpp
+++ PostfixArithmeticOperation/PostfixArithmeticOperation.cpp
@@ -8,72 +8,113 @@
 //|--------------------|
 
 #include <iostream>
-#include <sstream>
-#include <stack>
+
 using namespace std;
 
+int top=-1;
+
+class stackss
+{
+    public:
+
+        int stacks[100];
+        int isempty()
+        {
+            if(top==-1){return 1;}
+            else{return 0;}
+        }
+        void push(int data)
+        {
+                top++;
+                stacks[top]=data;
+        }
+        int pop()
+        {
+            int data;
+            data=stacks[top];
+            top--;
+            return data;
+        }
+};
 
 int main()
 {
     string input ;
     while(cin>>input)
     {
+        top=-1;
         int length_input;
-        long long int num;
-        long long int a,b;
+        int num;
+        int a,b;
         length_input = input.length();
-        stack<long long int> st;
+        stackss st;
         int flag =0;
         for(int j=0; j<length_input; j++)
         {
             if (input[j]=='+')
             {
-                a=st.top();
+                a=st.stacks[top];
                 st.pop();
-                if(!st.empty())
+                if(!st.isempty())
                 {
-                    b=st.top();
+                    b=st.stacks[top];
                     st.pop();
                     st.push(a+b);
                 }
-                else{flag +=1; goto Ans;}
+                else
+                {
+                    flag +=1;
+                    //goto Ans;
+                }
 
             }
             else if (input[j]=='-')
             {
-                a=st.top();
+                a=st.stacks[top];
                 st.pop();
-                if(!st.empty())
+                if(!st.isempty())
                 {
-                    b=st.top();
+                    b=st.stacks[top];
                     st.pop();
                     st.push(b-a);
                 }
-                else{flag +=1; goto Ans;}
+                else
+                {
+                    flag +=1;
+                    //goto Ans;
+                }
             }
             else if (input[j]=='*')
             {
-                a=st.top();
+                a=st.stacks[top];
                 st.pop();
-                if(!st.empty())
+                if(!st.isempty())
                 {
-                    b=st.top();
+                    b=st.stacks[top];
                     st.pop();
                     st.push(a*b);
                 }
-                else{flag +=1; goto Ans;}
+                else
+                {
+                    flag +=1;
+                    //goto Ans;
+                }
             }
             else if (input[j]=='/')
             {
-                a=st.top();
+                a=st.stacks[top];
                 st.pop();
-                if(!st.empty())
+                if(!st.isempty())
                 {
-                    b=st.top();
+                    b=st.stacks[top];
                     st.pop();
                     st.push(b/a);
                 }
-                else{flag +=1; goto Ans;}
+                else
+                {
+                    flag +=1;
+                    //goto Ans;
+                }
             }
             else
             {
@@ -82,14 +123,14 @@ int main()
             }
         }
 
-        Ans:
+//Ans:
         if(flag!=0)
         {
             cout<<"Input Error"<<endl;
         }
         else
         {
-            a=st.top();
+            a=st.stacks[top];
             cout <<a<<endl;
         }
 
